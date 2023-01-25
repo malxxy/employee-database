a3a6c7ceebf57799be9bb394d1ce57932e6ea65f
malxxy <maliayraguen@gmail.com>
2023-01-25 14:22:28 -0800

    Inquire.prompt questions for adding employee, department and role

--- server.js
+++ server.js
@@ -78,16 +78,48 @@ roleAdd = (userAnswer) => {
     con.connect(function(err) {
         if (err) throw err;
         console.log("Connected!");
-        con.query(`INSERT INTO roles (title,salary,department_id) VALUES (?)`,(userAnswer.addNewRole,userAnswer.addSalary,userAnswer.addID),function (err, result) {
-            if (err) throw(err);
-            console.log(result);
-            console.info("Added a table row");
+        con.query(`INSERT INTO roles (title,salary,department_id) VALUES (?)`,(userAnswer.addNewRole,userAnswer.addSalary,userAnswer.addID),function (err) {
+            console.info("Added a row to the role table");
         });
       });
 };
 
-// option to use switch in the future instead of if else. makes it shorter
-// future reference - turn into switch! more scalable and readable
+// first_name,last_name,role_id,manager_id
+const addEmployee = [
+    {
+        type: 'input',
+        message: 'What is the first name of the employee?',
+        name: 'addFirst',
+    },
+    {
+        type: 'input',
+        message: 'What is the last name of the employee?',
+        name: 'addLast',
+    },
+    {
+        type: 'input',
+        message: 'What is the id of the role (1,2,3,4,5)?',
+        name: 'addRoleID',
+    },
+    {
+        type: 'input',
+        message: 'What is the id of the role (1 or 2)?',
+        name: 'addManagerID',
+    }
+]
+
+employeeAdd = (userAnswer) => {
+    console.log(userAnswer);
+    con.connect(function(err) {
+        if (err) throw err;
+        console.log("Connected!");
+        con.query(`INSERT INTO roles (first_name,last_name,role_id,manager_id) VALUES (?)`,(userAnswer.addFirst,userAnswer.addLast,userAnswer.addRoleID,userAnswer.addManagerID),function (err) {
+            console.info("Added a row to the employee table");
+        });
+      });
+};
+
+// option to use switch in the future instead of if else. makes it shorter // future reference - turn into switch! more scalable and readable
 // If user selects this choice, then computer will show data or ask more questions
 function showResults(userChoice) {
     if (userChoice.choices === 'view all departments') {
@@ -108,7 +140,7 @@ function showResults(userChoice) {
         inquirer.prompt(addRole).then((userAnswer) => roleAdd(userAnswer));
     } else if (userChoice.choices === 'add an employee') {
         console.log('user chose to add an employee');
-        // logic to add an employee
+        inquirer.prompt(addEmployee).then((userAnswer) => employeeAdd(userAnswer));
     } else if (userChoice.choices === 'update an employee role') {
         console.log('user chose to update a role');
         // logic to update an employee role
