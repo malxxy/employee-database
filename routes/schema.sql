a79ac6efa94f0417c3b95cc4f9d28a18907d5a01
malxxy <maliayraguen@gmail.com>
2023-01-25 11:38:57 -0800

    Logic to add a department function

--- routes/schema.sql
+++ routes/schema.sql
@@ -4,22 +4,12 @@ CREATE DATABASE employees_db;
 USE employees_db;
 
 CREATE TABLE department (
-  id INT NOT NULL PRIMARY KEY,
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   name VARCHAR(30) NOT NULL
 );
 
-CREATE TABLE roles (
-  id INT NOT NULL PRIMARY KEY,
-  title VARCHAR(30) NOT NULL,
-  salary DECIMAL,
-  department_id INT,
-  FOREIGN KEY (deparmtnet_id)
-  REFERENCES employee(id)
-  ON DELETE SET NULL
-);
-
 CREATE TABLE employee (
-  id INT PRIMARY KEY,
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
   role_id INT,
@@ -32,10 +22,21 @@ CREATE TABLE employee (
   ON DELETE SET NULL-- `INT` to hold reference to another employee that is the manager of the current employee (`null` if the employee has no manager)
 );
 
+
+CREATE TABLE roles (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  title VARCHAR(30) NOT NULL,
+  salary DECIMAL,
+  department_id INT,
+  FOREIGN KEY (deparmtnet_id)
+  REFERENCES employee(id)
+  ON DELETE SET NULL
+);
+
 show tables;
 
-describe employee;
+describe department;
 
 describe roles;
 
-describe department;
+describe employee;
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
-const inquirer = require('./node_modules/inquirer'); // require inquirer
-const mysql = require('./node_modules/mysql2'); // require sql
-const express = require('./node_modules/express'); // require express
+const inquirer = require('inquirer'); 
+// const inquire = require('./node_modules/inquire');
+const mysql = require('mysql2'); // require sql
+const express = require('express'); // require express
 
 const PORT = process.env.PORT || 3001; // local host
 const app = express(); // launch express app
@@ -12,30 +13,16 @@ const con = mysql.createConnection({
   host: "127.0.0.1",
   user: "root",
   password: "codecode123",
-  database: "employees.db"
+  database: "employees_db"
+});
+  
+app.use((req, res) => {
+res.status(404).end();
 });
 
-// db.query('SELECT COUNT(id) AS total_count FROM favorite_books GROUP BY in_stock', function (err, results) {
-//     console.log(results);
-//     // count number of favorite books that are in stock and list IDs
-//     // count id from total count and group by favorite books in stock
-//     // querying database using count method and group by IN STOCK
-//   });
-  
-//   db.query('SELECT SUM(quantity) AS total_in_section, MAX(quantity) AS max_quantity, MIN(quantity) AS min_quantity, AVG(quantity) AS avg_quantity FROM favorite_books GROUP BY section', function (err, results) {
-//     console.log(results);
-//     // sum quantity of favorite books in sections
-//     // list max, min, and average
-//   });
-  
-//   app.use((req, res) => {
-//     res.status(404).end();
-//   });
-  
-//   app.listen(PORT, () => {
-//     console.log(`Server running on port ${PORT}`);
-//   });
-
+app.listen(PORT, () => {
+console.log(`Server running on port ${PORT}`);
+});
 
 // Array of questions for user input
 const questions = [
@@ -55,14 +42,15 @@ const addDepartment = [
     }
 ]
 
-departmentAdd => {
+departmentAdd = (userAnswer) => {
     con.connect(function(err) {
         if (err) throw err;
         console.log("Connected!");
-        var addARow = `INSERT INTO department (id,name) VALUES (${addDepartment.input})`;
+        var addARow = `INSERT INTO department (id,name) VALUES (6, ${userAnswer.input})`;
         con.query(addARow, function (err, result) {
-          if (err) throw err;
-          console.log("Table created");
+            if (err) throw(err);
+            console.log(result);
+            console.info("Added a table row");
         });
       });
 }
@@ -71,19 +59,25 @@ departmentAdd => {
 function showResults(userChoice) {
     if (userChoice.choices === 'view all departments') {
         console.log('user chose to view all departments');
+        // add logic to show all departments
     } else if (userChoice.choices === 'view all roles') {
         console.log('user chose to view all roles');
+        // add logic to view all roles
     } else if (userChoice.choices === 'view all employees') {
         console.log('user chose to view all emplyes');
+        // add logic to view all employees
     } else if (userChoice.choices === 'add a department') {
-        console.log('user chose to add a department');
-        inquirer.prompt(addDepartment).then(addDepartment => departmentAdd());
+        console.log("user chose to add a department");
+        inquirer.prompt(addDepartment).then((userAnswer) => departmentAdd(userAnswer));
     } else if (userChoice.choices === 'add a role') {
         console.log('user chose to add a role');
+        // logic to add a role
     } else if (userChoice.choices === 'add an employee') {
         console.log('user chose to add an employee');
+        // logic to add an employee
     } else if (userChoice.choices === 'update an employee role') {
         console.log('user chose to update a role');
+        // logic to update an employee role
     } else if (userChoice.choices === 'quit') {
         console.log('user chose to return ot home page');
         init();
